57651a03cbd9702ef6bf00c47ca890dfa7dc69d8
Brook Xu <33502952+Brook-Xu@users.noreply.github.com>
2023-12-21 07:11:07 +0800

    Create facility.js

---
+++ facility.js
@@ -0,0 +1,21 @@
+// ==UserScript==
+// @name         jiangsu province hospital core facility center
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the jiangsu province hospital core facility center!
+// @author       Bondxu
+// @match        http://yqgx.jsph.org.cn/lfsms/personbook/
+// @icon         http://yqgx.jsph.org.cn/upload/logo/1589251751clbqv.png
+// @grant        none
+// @run-at document-end
+// @require https://code.jquery.com/jquery-2.1.4.min.js
+// @require tampermonkey://vendor/jquery.js
+// @require tampermonkey://vendor/jszip/jszip.js
+
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    
+    // Your code here...
+})();
